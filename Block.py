7bfa99449c63747a95d1d789f268440c5e5bfc1d
Bhushan <bhushan1810patil@gmail.com>
2021-03-08 20:11:47 +0530

    added draw to Block

--- Block.py
+++ Block.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame import Surface
+from settings import *
 
 class Block:
 
@@ -8,4 +10,12 @@ class Block:
         self.size = size
         self.x = self.col * self.size
         self.y = self.row * self.size
+        self.color = COLORS[(0 if self.row % 2 == 0 else 1 + self.col) % 8]
+        self.occupied = False
+
+    def draw(self, win: Surface):
+        rect = (self.x, self.y, self.size, self.size)
+        pygame.draw.rect(win, self.color, rect, 0)
     
+    def isOccupied(self) -> bool:
+        return self.occupied
